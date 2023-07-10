faf26ef2460b7e531da6332279ca9d406f1987ff
dongeranguk <rooetym@naver.com>
2023-07-10 23:53:56 +0900

    #17 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 a0b8daa 에서 확인하였으므로,
    나머지 뷰도 동일하게 부가적인 옵션이 예상되어
    미리 테스트 메소드 업데이트

--- wise-groupware/src/test/java/com/wise/groupware/controller/ArticleControllerTest.java
+++ wise-groupware/src/test/java/com/wise/groupware/controller/ArticleControllerTest.java
@@ -45,7 +45,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("article/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -61,7 +61,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("articles/search"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 
     @Disabled("구현 중")
@@ -73,7 +73,7 @@ class ArticleControllerTest {
 
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"))
                 .andExpect(status().isOk());
     }
